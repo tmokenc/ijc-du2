7864eb46a4561f3ee3ecf1b25cd885f0ade1ddd5
tmokenc <tmokenc@protonmail.com>
2022-04-16 17:07:48 +0900

    init

---
+++ htab_init.c
@@ -0,0 +1,41 @@
+/**
+ * htab_init.c
+ * Řešení IJC-DU2, 3. 4. 2022
+ * Autor: tmokenc, FIT
+ * Přeloženo: gcc 11.2
+ */
+ 
+#include <stdlib.h>
+#include <stdio.h>
+#include "htab.h"
+#include "htab_private.h"
+
+htab_t *htab_init(size_t n) {
+    if (n == 1) {
+        fprintf(stderr, "n have to be at least 1");
+        return NULL;
+    }
+    
+    htab_t *data = malloc(sizeof(htab_t));
+    
+    if (data == NULL) {
+        fprintf(stderr, "Out Of Memory");
+        return NULL;
+    }
+    
+    data.size = 0;
+    data.arr_size = n;
+    data.arr_ptr = malloc(sizeof(struct htab_item *) * n);
+    
+    if (data.arr_ptr == NULL) {
+        fprintf(stderr, "Out Of Memory");
+        free(data);
+        return NULL;
+    }
+    
+    for (size_t i = 0; i < n; i++) {
+        data.arr_ptr[i] = NULL;
+    }
+    
+    return data;
+}
